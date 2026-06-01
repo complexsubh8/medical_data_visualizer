5a7f263e3a784a2ee79cab55b75a8c4c5da444f6
Subham Sarkar <149823541+complexsubh8@users.noreply.github.com>
2026-06-01 12:24:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from medical_data_visualizer import draw_cat_plot, draw_heat_map
+
+draw_cat_plot().savefig('catplot.png')
+draw_heat_map().savefig('heatmap.png')
+
+print("Plots generated successfully!")
